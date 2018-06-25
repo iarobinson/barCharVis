6cf92acd83100788c46332068c830d350c9c8cb1
Ian <ian (at) ianrobinson.net>
2018-06-25 11:38:42 -0700

    This is a mess of trying to use the code of others in my bar graph... ugh

--- js/barChartVis.js
+++ js/barChartVis.js
@@ -1,6 +1,5 @@
+var simpleGDPData = [], json;
 window.onload = init;
-var simpleGDPData = [];
-var json;
 
 function init() {
   // Get data from Free Code Camp API - Start
@@ -11,11 +10,110 @@ function init() {
   req.send();
   req.onload = function() {
     json = JSON.parse(req.responseText);
-    // dataSpot.innerHTML = `Data loaded from the ${json.source_name} ranging from year ${json.from_date.slice(0, 4)} to ${json.to_date.slice(0, 4)}.`;
-
     // Simplify data to a single array of GDP nominal values and assign to a global variable
     json.data.forEach(function(e) { simpleGDPData.push(e[1]) });
     renderGraph();
   }
   // Get data from Free Code Camp API - End
+}
+
+// Rendering bar chart with D3.js
+function renderGraph() {
+  //NOTED
+  // Setting up the graph SVG with variables that are easily changed
+  const yMargin = 40;
+  const width = 800;
+  const height = 400;
+  const barWidth = width / 275;
+
+  // Using D3.js to pull max values for year and GDP of Fed Reserve data
+  const minGDP = d3.min(simpleGDPData);
+  const maxGDP = d3.max(simpleGDPData);
+  const minYear = d3.min(json.data, (d) => d[0].slice(0, 4));
+  const maxYear = d3.max(json.data, (d) => d[0].slice(0, 4));
+
+  //noted
+  const svg = d3.select("#body")
+                .append("svg")
+                .attr("width", width + 100)
+                .attr("height", height + 60);
+
+  //noted
+  const xScale = d3.scaleLinear()
+                  .domain([minYear, maxYear])
+                  .range([0, width]);
+  //noted
+  const linearScale = d3.scaleLinear()
+                        .domain([minGDP, maxGDP])
+                        .range([(minGDP / maxGDP) * height, height]);
+
+  //noted
+  const scaledGDP = simpleGDPData.map(function(item) {
+    return linearScale(item);
+  })
+
+  //noted
+  const yAxisScale = d3.scaleLinear()
+                       .domain([minGDP, maxGDP])
+                       .range([height, (minGDP / maxGDP) * height]);
+
+  //noted
+  const yAxis = d3.axisLeft(yAxisScale);
+  //noted
+  const yAxisGroup = svg.append("g")
+                        .call(yAxis)
+                        .attr("id", "y-axis")
+                        .attr("transform", "translate(60, 0)");
+
+  const overlay = d3.select('svg').append('div')
+  .attr('class', 'overlay')
+  .style('opacity', 0);
+
+  const tooltip = d3.select(".visHolder")
+                  .append("div")
+                  .attr("id", "tooltip")
+                  .style("opacity", 0);
+
+  d3.select("svg")
+    .selectAll("rect") //
+    .data(scaledGDP) // 
+    .enter() //
+    .append("rect") //
+    .attr("x", (d, i) => i * barWidth)
+    .attr("y", (d, i) => height - d)
+    .attr("width", barWidth)
+    .attr("height", (d) => d)
+    .attr("transform", "translate(60, 0)")
+    .attr("data-date", (d, i) => json.data[i][0])
+    .attr("data-gdp", (d, i) => json.data[i][1])
+    .attr("class", "bar")
+    // .attr("id", "tooltip")
+    .on('mouseover', function(d, i) {
+      overlay.transition()
+        .duration(0)
+        .style('height', d + 'px')
+        .style('width', barWidth + 'px')
+        .style('opacity', .9)
+        .style('left', (i * barWidth) + 0 + 'px')
+        .style('top', height - d + 'px')
+        .style('transform', 'translateX(60px)');
+      tooltip.transition()
+        .duration(200)
+        .style('opacity', .9);
+      tooltip.html(json.data[i][0].slice(0, 4) + '<br>' + '$' + simpleGDPData[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') + ' Billion')
+        .attr('data-date', json.data[i][0])
+        .style('left', (i * barWidth) + 30 + 'px')
+        .style('top', height - 100 + 'px')
+        .style('transform', 'translateX(60px)');
+    })
+
+  //noted
+  const xAxis = d3.axisBottom()
+                  .scale(xScale)
+                  .tickFormat(d3.format("d"));
+ //noted
+ const xAxisGroup = svg.append("g")
+                       .call(xAxis)
+                       .attr("id", "x-axis")
+                       .attr("transform", "translate(60, 400)");
 }
